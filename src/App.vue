59fd0c9483167821aa2329aedde76ee7c995edac
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-05 20:34:09 +0800

    添加文件夹

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,41 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
 import FileFloder from './components/FileFloder.tsx';
+const imgClick = () => {
+  console.log('img');
+};
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
   <FileFloder>
     <img
-      style="width: 36px; height: 36px"
-      src="https://api.iconify.design/bi:1-square-fill.svg?color=%23409eff"
+      @click="imgClick"
+      src="https://api.iconify.design/bi:1-square-fill.svg?color=%23999999"
       alt=""
     />
     <img
-      style="width: 36px; height: 36px"
-      src="https://api.iconify.design/bi:2-square-fill.svg?color=%23409eff"
+      src="https://api.iconify.design/bi:2-square-fill.svg?color=%23999999"
       alt=""
     />
     <img
-      style="width: 36px; height: 36px"
-      src="https://api.iconify.design/bi:3-square-fill.svg?color=%23409eff"
+      src="https://api.iconify.design/bi:3-square-fill.svg?color=%23999999"
+      alt=""
+    />
+    <img
+      src="https://api.iconify.design/bi:3-square-fill.svg?color=%23999999"
+      alt=""
+    />
+    <img
+      src="https://api.iconify.design/bi:3-square-fill.svg?color=%23999999"
+      alt=""
+    />
+    <img
+      src="https://api.iconify.design/bi:3-square-fill.svg?color=%23999999"
+      alt=""
+    />
+    <img
+      src="https://api.iconify.design/bi:3-square-fill.svg?color=%23999999"
       alt=""
     />
   </FileFloder>
@@ -28,9 +45,31 @@ import FileFloder from './components/FileFloder.tsx';
 
 <style lang="scss">
 .fileFloder {
+  width: 36px;
+  height: 36px;
+  user-select: none;
+  &-container {
+    width: 180px;
+    height: 180px;
+    display: inline-grid;
+    padding: 16px;
+    grid-gap: 16px;
+    transform-origin: left top;
+    transform: scale(0.2);
+    border-radius: 16px;
+    background-color: #e5e5e5;
+    grid-template-rows: repeat(3, 1fr);
+  }
   &-drag {
     display: inline-block;
     cursor: pointer;
+    width: 100%;
+    height: 100%;
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
--- src/components/FileFloder.tsx
+++ src/components/FileFloder.tsx
@@ -1,14 +1,64 @@
-import { defineComponent, h } from 'vue';
+import {
+  defineComponent,
+  h,
+  ref,
+  reactive,
+  watchEffect,
+  withModifiers,
+} from 'vue';
 
 export default defineComponent({
   setup(props, { slots }) {
+    const canClick = ref(false);
+    let links = new Array(7).fill({
+      name: '微博',
+      icon: 'https://weibo.com/favicon.ico',
+      link: 'https://weibo.com/u/5963931367/home?wvr=5',
+    });
+    // 图片点击
+    const imgClick = (item: any) => {
+      canClick.value = false;
+      console.log('img');
+    };
+    // 文件夹点击
+    const folderClick = () => {
+      if (!canClick.value) {
+        canClick.value = true;
+      }
+      console.log('folder');
+    };
+    const style = reactive({
+      folder: 'fileFloder',
+      container: `grid-template-columns:repeat(3, 1fr)`,
+    });
+    watchEffect(() => {
+      if (canClick.value) {
+        style.folder = 'width: 108px;';
+        style.container = `grid-template-columns:repeat(6, 1fr);width: 540px`;
+      } else {
+        style.folder = 'cursor: pointer;';
+        style.container = `grid-template-columns:repeat(3, 1fr);`;
+      }
+    });
     return () => {
       let defaultEles = slots.default ? slots.default() : [];
       return (
-        <div>
-          <div class="fileFloder">
-            {defaultEles.map((ele) => {
-              return h('div', { class: 'fileFloder-drag' }, [h(ele, {}, {})]);
+        <div class="fileFloder" style={style.folder} onClick={folderClick}>
+          <div class="fileFloder-container" style={style.container}>
+            {links.map((ele) => {
+              return h('div', { class: 'fileFloder-drag' }, [
+                h(
+                  'img',
+                  {
+                    onClick: withModifiers(
+                      () => canClick.value && imgClick(ele),
+                      canClick.value ? ['stop', 'capture'] : []
+                    ),
+                    src: ele.icon,
+                  },
+                  {}
+                ),
+              ]);
             })}
           </div>
         </div>
