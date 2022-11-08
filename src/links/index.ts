c632c39c5cee52d1682873b42ff1986c8ddea87b
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-08 13:48:19 +0800

    添加 收藏链接

---
+++ src/links/index.ts
@@ -0,0 +1,14 @@
+interface Link {
+  url: string;
+  name: string;
+}
+
+const juejin: Link[] = [
+  {
+    name: 'avascript技能树',
+    url: 'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/a03678b442394d1fa02a0e9e6e8aa939~tplv-k3u1fbpfcp-watermark.image?',
+  },
+];
+export default {
+  juejin,
+};
