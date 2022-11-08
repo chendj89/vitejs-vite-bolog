3906a9f9ba6758a40eb40a495219082931d50612
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-08 13:53:43 +0800

    添加vue、react文档链接

--- src/links/index.ts
+++ src/links/index.ts
@@ -8,6 +8,14 @@ const juejin: Link[] = [
     name: 'avascript技能树',
     url: 'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/a03678b442394d1fa02a0e9e6e8aa939~tplv-k3u1fbpfcp-watermark.image?',
   },
+  {
+    name: 'Vue技能树',
+    url: 'https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7dcd1a9e7fb24884b31ab233089a77bf~tplv-k3u1fbpfcp-watermark.image?',
+  },
+  {
+    name: 'React技能树',
+    url: 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/b64f19a23608425fbb61236704e07efa~tplv-k3u1fbpfcp-watermark.image?',
+  },
 ];
 export default {
   juejin,
