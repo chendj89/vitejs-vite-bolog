7c1c7b9e7f840ac944cce86e13373f9bd08be2db
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-08 20:36:29 +0800

    基础按钮

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@ const imgClick = () => {
 
 <template>
   <HelloWorld />
+  <div class="cc-button" tabindex="0" role="button">按钮</div>
+  <div
+    style="margin-left: 20px"
+    class="cc-btn cc-primary"
+    tabindex="0"
+    role="button"
+  >
+    按钮
+  </div>
 </template>
 
 <style lang="scss">
@@ -65,6 +74,68 @@ const imgClick = () => {
     height: 100%;
     img {
       display: block;
+    }
+  }
+}
+.cc {
+  &-button {
+    --theme: #4676d7;
+    --theme-hover: dark(#4676d7, 0.1);
+    --color: #fff;
+    display: inline-block;
+    text-align: center;
+    min-width: 100px;
+    font-family: inherit;
+    appearance: none;
+    border: 0;
+    border-radius: 5px;
+    background: var(--theme);
+    color: var(--color);
+    padding: 8px 16px;
+    font-size: 1rem;
+    cursor: pointer;
+    &:focus {
+      color: #fff;
+      background-color: #0b5ed7;
+      border-color: #0a58ca;
+      box-shadow: 0 0 0 0.25rem rgb(49 132 253 / 50%);
+    }
+  }
+  &-btn {
+    display: inline-block;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    :not(:disabled) {
+      cursor: pointer;
+    }
+  }
+  &-primary {
+    color: #fff;
+    background-color: #0d6efd;
+    border-color: #0d6efd;
+    &:hover {
+      color: #fff;
+      background-color: #0b5ed7;
+      border-color: #0a58ca;
+    }
+    &:focus {
+      color: #fff;
+      background-color: #0b5ed7;
+      border-color: #0a58ca;
+      box-shadow: 0 0 0 0.25rem rgb(49 132 253 / 50%);
     }
   }
 }
