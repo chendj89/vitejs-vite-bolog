7365a035ee15698b76cc4860bdba2bba3f658f5d
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-09 11:33:47 +0800

    添加md支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import Markdown from 'vite-plugin-md';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vueJsx(), vue()],
+  plugins: [
+    vueJsx(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown(),
+  ],
 });
