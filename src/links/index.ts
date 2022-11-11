515c100828559050e246933d9e4a75a24916feb2
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-11 15:28:30 +0800

    更新掘金

--- src/links/index.ts
+++ src/links/index.ts
@@ -16,6 +16,10 @@ const juejin: Link[] = [
     name: 'React技能树',
     url: 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/b64f19a23608425fbb61236704e07efa~tplv-k3u1fbpfcp-watermark.image?',
   },
+  {
+    name:"花了一天的时间，地板式扫盲了vue3所有API盲点",
+    url:"https://juejin.cn/post/7164159759619194893"
+  }
 ];
 export default {
   juejin,
